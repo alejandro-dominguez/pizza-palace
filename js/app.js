8f0ebee55336c4a3a8b6f004f8f542050b71fac5
alejandro-dominguez <alejandro.dominguez1@live.com>
2022-09-12 17:22:38 -0300

    proyecto final JS

---
+++ js/app.js
@@ -0,0 +1,221 @@
+const cardsContainer = document.querySelector("#cards-container");
+const cartItemsContainer = document.querySelector("#cart-items-container");
+const cartTotal = document.querySelector("#cartTotal");
+const deleteCartBtn = document.querySelector("#delete-cart");
+const buyCartBtn = document.querySelector(".cart-check");
+const openCartBtn = document.querySelector("#open-cart-btn");
+const loadingSpinner = document.querySelector(".spinner-container");
+let products = [];
+let cart = {};
+
+// data fecht //
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchData()
+});
+
+const fetchData = async () => {
+    await fetch("https://6313ca7efc9dc45cb4e63edf.mockapi.io/api/v1/products")
+    .then((response) => response.json())
+    .catch((error) => {
+        console.error("Hubo un problema al cargar los productos", error)
+        loadingSpinner.style.display = "block"
+    })
+    .then((json) => {
+        loadingSpinner.style.display = "block"
+        setTimeout(() => {
+            loadingSpinner.style.display = "none"
+            let products = json
+            createCards(products)
+            addToCart(products)
+        }, 8000)
+    })
+}
+
+// generate product cards and modals //
+
+const createCards = (products) => {
+    const cardsTemplate = document.querySelector("#cards-template").content
+    const cardsFragment = document.createDocumentFragment()
+
+    products.forEach(product => {
+        cardsTemplate.querySelector(".menu-card").setAttribute("data-bs-target", `#${product.name}`)
+        cardsTemplate.querySelector(".menu-card-title").textContent = product.name
+        cardsTemplate.querySelector(".menu-card-p").textContent = product.description
+        cardsTemplate.querySelector(".menu-card-img").setAttribute("src", product.img)
+        cardsTemplate.querySelector(".menu-card-img").setAttribute("alt", product.alt)
+        cardsTemplate.querySelector(".modal").setAttribute("id", product.name)
+        cardsTemplate.querySelector(".modal").setAttribute("aria-labelledby", `${product.name}Label`)
+        cardsTemplate.querySelector(".modals-img").setAttribute("src", product.img)
+        cardsTemplate.querySelector(".modals-img").setAttribute("alt", product.alt)
+        cardsTemplate.querySelector(".modals-title").textContent = product.name
+        cardsTemplate.querySelector(".modals-p").textContent = product.description
+        cardsTemplate.querySelector("p span").textContent = product.price
+        cardsTemplate.querySelector(".modals-check").dataset.id = product.id
+        
+        const cardsClone = cardsTemplate.cloneNode(true)
+        cardsFragment.appendChild(cardsClone)
+    })
+
+    cardsContainer.appendChild(cardsFragment)
+    buyCartBtn.disabled = true
+    deleteCartBtn.disabled = true
+}
+
+// add to cart buttons //
+
+const addToCart = (products) => {
+    const btns = document.querySelectorAll(".modals-check")
+    btns.forEach(btn => {
+        btn.addEventListener("click", ()=> {
+        const product = products.find(prod => prod.id === Number(btn.dataset.id))
+        product.amount = 1
+        if (cart.hasOwnProperty(product.id)) {
+            product.amount = cart[product.id].amount + 1
+        }
+        cart[product.id] = {...product}
+        refreshCart()
+        openCartBtn.classList.add("buy")
+        setTimeout(() => {
+            Swal.fire({
+                customClass: {
+                title: "title-swal-products"},
+                width: "fit-content",
+                position: "top",
+                background: "#e8cc41",
+                color: "#191919",
+                toast: true,
+                title: "¡PRODUCTO AGREGADO! &#10004;",
+                showConfirmButton: false,
+                timer: 1250,
+                allowEscapeKey: false,
+                stopKeydownPropagation: true
+                })
+            }, 300)
+        })
+    })
+}
+
+// cart items rendering //
+
+const refreshCart = () => {
+    cartItemsContainer.innerHTML = ""
+//    addLocalStorage()
+//    cartToLocalStorage()
+
+    if (Object.keys(cart).length === 0){
+        cartItemsContainer.innerHTML = `<p class="cart-item-container-p">carrito vacío<br>¡Comienza a comprar!</p>`
+        cartTotal.innerText = 0
+        openCartBtn.classList.remove("buy")
+        cart = {}
+        return
+    }
+
+    const cartItemTemplate = document.querySelector("#cart-item-template").content
+    const cartItemFragment = document.createDocumentFragment()
+    const cartProducts = Object.values(cart)
+
+    cartProducts.forEach(product => {
+        cartItemTemplate.querySelector(".cart-item-img").setAttribute("src", product.img)
+        cartItemTemplate.querySelector("img").setAttribute("alt", product.alt)
+        cartItemTemplate.querySelector(".cart-item-title").textContent = product.name
+        cartItemTemplate.querySelector(".cart-item-amount").textContent = product.amount
+        cartItemTemplate.querySelector("p span").textContent = product.amount * product.price
+        cartItemTemplate.querySelector(".cart-item-plus").dataset.id = product.id
+        cartItemTemplate.querySelector(".cart-item-minus").dataset.id = product.id
+        cartItemTemplate.querySelector(".cart-item-delete").dataset.id = product.id
+
+        const cartItemClone = cartItemTemplate.cloneNode(true)
+        cartItemFragment.appendChild(cartItemClone)
+    })
+
+    cartItemsContainer.appendChild(cartItemFragment)
+
+    cartControllers()
+    cartItemControllers()
+}
+
+// cart buttons //
+
+const cartControllers = () => {
+    const itemsTotal = Object.values(cart).reduce((acc, {amount, price}) => acc + amount * price, 0)
+    cartTotal.innerText = itemsTotal
+    buyCartBtn.disabled = false
+
+    deleteCartBtn.addEventListener("click", ()=> {
+        cart = {}
+        refreshCart()
+        buyCartBtn.disabled = true
+        openCartBtn.classList.remove("buy")
+    })
+    buyCartBtn.addEventListener("click", ()=> {
+        cart = {}
+        refreshCart()
+        cartItemsContainer.innerHTML = `<p class="cart-item-container-p">¡Gracias por tu compra!</p>`
+        buyCartBtn.disabled = true
+        openCartBtn.classList.remove("buy")
+        setTimeout(() => {
+        Swal.fire({
+            customClass: {
+            title: "title-swal"},
+            icon: "success",
+            iconColor: "rgba(255, 255, 255, 0.8)",
+            background: "#d61a28",
+            color: '#fff',
+            title: "¡Gracias por tu compra! En breve va a llegar tu pedido.",
+            imageUrl: "./images/logo-nav.svg",
+            imageAlt: "logo pizza palace",
+            imageWidth: "8rem",
+            showConfirmButton: false,
+            timer: 3000,
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            allowEnterKey: false,
+            stopKeydownPropagation: true
+            })
+        }, 400)
+    })
+}
+
+// cart items buttons //
+
+const cartItemControllers = () => {
+    const addBtns = document.querySelectorAll(".cart-item-container .cart-item-plus")
+    const removeBtns = document.querySelectorAll(".cart-item-container .cart-item-minus")
+    const deleteBtns = document.querySelectorAll(".cart-item-container .cart-item-delete")
+
+    addBtns.forEach(btn => {
+        btn.addEventListener("click", ()=>{
+            const product = cart[btn.dataset.id]
+            product.amount ++
+            cart[btn.dataset.id] = {...product}
+            refreshCart()
+        })
+    })
+    removeBtns.forEach(btn => {
+        btn.addEventListener("click", ()=>{
+            const product = cart[btn.dataset.id]
+            product.amount --
+            if (product.amount === 0) {
+                delete cart[btn.dataset.id]
+                buyCartBtn.disabled = true
+                openCartBtn.classList.remove("buy")
+            } else {
+                cart[btn.dataset.id] = {...product}
+            }
+            refreshCart()
+        })        
+    })
+    deleteBtns.forEach(btn => {
+        btn.addEventListener("click", ()=>{
+            const product = cart[btn.dataset.id]
+            product.amount = 0
+            delete cart[btn.dataset.id]
+            buyCartBtn.disabled = true
+            if (cart.length === 0) {
+                openCartBtn.classList.remove("buy")
+            }
+            refreshCart()
+        })        
+    })
+}
---
+++ js/dom.js
@@ -0,0 +1,174 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navigation-menu");
+const carousel = document.querySelector(".header-carousel");
+const btnRight = document.querySelector(".header-carousel-btn-right");
+const btnLeft = document.querySelector(".header-carousel-btn-left");
+const slider = document.querySelector(".contact-slider");
+let carouselItem = document.querySelectorAll(".header-carousel-item");
+let carouselItemLast = carouselItem[carouselItem.length -1];
+let sliderItem = document.querySelectorAll(".contact-slider-item");
+let sliderItemLast = sliderItem[sliderItem.length -1];
+const fadeLeft = document.querySelector(".fade-in-left");
+const fadeRight = document.querySelector(".fade-in-right");
+const options = {threshold: 0, rootMargin: "-150px"};
+const navDarkBtn = document.querySelector(".navigation-dark");
+const navLightBtn = document.querySelector(".navigation-light");
+const htmlBody = document.querySelector("body");
+const aboutImgL = document.querySelector(".about-container-img");
+const aboutImgS = document.querySelector(".about-img");
+const menuImg = document.querySelector(".menu-img");
+const contactImgL = document.querySelector(".contact-container-img");
+const contactImgS = document.querySelector(".contact-img");
+const nav = document.querySelector(".navigation");
+const navMenuDark = document.querySelector(".navigation-menu");
+const cartBtn = document.querySelector(".cart-link");
+const cartContainer = document.querySelector(".offcanvas");
+const cartTitle = document.querySelector(".cart-title");
+const cartCloseBtn = document.querySelector(".cart-close");
+const cartDeleteBtn = document.querySelector(".cart-delete");
+const cartItemsWrapper = document.querySelector(".cart-item-container");
+const footer = document.querySelector(".footer");
+const navLinks = document.querySelectorAll(".navigation-link");
+const colorModeItems = [htmlBody, aboutImgL, aboutImgS, menuImg, contactImgL, contactImgS, nav, navMenuDark,
+cartBtn, cartContainer, cartCloseBtn, cartTitle, cartDeleteBtn, cartItemsWrapper, footer];
+
+// responsive menu //
+  
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active")
+    navMenu.classList.toggle("active")
+});
+
+document.querySelectorAll(".navigation-link").forEach(e => e.addEventListener("click", () => {
+    hamburger.classList.remove("active")
+    navMenu.classList.remove("active")
+}));
+
+document.querySelector(".navigation-logo").addEventListener("click", () => {
+    hamburger.classList.remove("active")
+    navMenu.classList.remove("active")
+});
+
+// carousel //
+
+carousel.insertAdjacentElement("afterbegin", carouselItemLast)
+
+const next = () => {
+    let carouselItemFirst = document.querySelectorAll(".header-carousel-item")[0]
+    carousel.style.marginLeft = "-200%"
+    carousel.style.transition = "all 1s"
+    setTimeout(() => {
+        carousel.style.transition = "none"
+        carousel.insertAdjacentElement("beforeend", carouselItemFirst)
+        carousel.style.marginLeft = "-100%"
+    }, 1500)
+}
+
+const prev = () => {
+    let carouselItem = document.querySelectorAll(".header-carousel-item")
+    let carouselItemLast = carouselItem[carouselItem.length -1]
+    carousel.style.marginLeft = "0"
+    carousel.style.transition = "all 1s"
+    setTimeout(() => {
+        carousel.style.transition = "none"
+        carousel.insertAdjacentElement("afterbegin", carouselItemLast)
+        carousel.style.marginLeft = "-100%"
+    }, 1500)
+}
+
+btnRight.addEventListener("click", next);
+btnLeft.addEventListener("click", prev);
+
+// slider //
+
+slider.insertAdjacentElement("afterbegin", sliderItemLast)
+
+const nextSlide = () => {
+    let sliderItemFirst = document.querySelectorAll(".contact-slider-item")[0]
+    slider.style.marginLeft = "-200%"
+    slider.style.transition = "all 1s"
+    setTimeout(() => {
+        slider.style.transition = "none"
+        slider.insertAdjacentElement("beforeend", sliderItemFirst)
+        slider.style.marginLeft = "-100%"
+    }, 3000)
+}
+
+setInterval((nextSlide), 6000);
+
+// animations //
+
+const fadeLeftOnScroll = new IntersectionObserver((entries, fadeLeftOnScroll) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return
+        } else {
+            entry.target.classList.toggle("appear")
+            fadeLeftOnScroll.unobserve(entry.target)
+        }
+    })
+}, options);
+
+const fadeRightOnScroll = new IntersectionObserver((entries, fadeRightOnScroll) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return
+        } else {
+            entry.target.classList.toggle("appear")
+            fadeRightOnScroll.unobserve(entry.target)
+        }
+    })
+}, options);
+
+fadeLeftOnScroll.observe(fadeLeft);
+fadeRightOnScroll.observe(fadeRight);
+
+// color mode //
+
+const darkMode = () => {
+    navDarkBtn.addEventListener("click", () => {
+        colorModeItems.forEach(item => {
+        item.classList.add("dark")    
+        })
+        navLinks.forEach(link => {
+        link.classList.add("dark")  
+        })
+        localStorage.setItem("mode", "dark")
+    })
+}
+
+const lightMode = () => {
+    navLightBtn.addEventListener("click", () => {
+        colorModeItems.forEach(item => {
+        item.classList.remove("dark")    
+        })
+        navLinks.forEach(link => {
+        link.classList.remove("dark")  
+        })
+        localStorage.setItem("mode", "light")
+    })
+}
+
+const saveMode = () => {
+    if (localStorage.getItem("mode") === ("dark")) {
+        colorModeItems.forEach(item => {
+        item.classList.add("dark")    
+        })
+        navLinks.forEach(link => {
+        link.classList.add("dark")  
+        })
+        
+    } else {
+        colorModeItems.forEach(item => {
+        item.classList.remove("dark")    
+        })
+        navLinks.forEach(link => {
+        link.classList.remove("dark")  
+        })
+     }
+}
+
+darkMode();
+lightMode();
+saveMode();
+
